9354c1812fbaea09e4a85c98ee01a16ed224e41b
samvrny <samvrny@gmail.com>
2023-06-21 11:49:15 -0500

    feature/add-content

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -3,11 +3,9 @@
         <h2>Active Notes, Reminders, and Ideas</h2>
         <p>📌Add Webfontloader to this website so I can add a prefferable font that I like to look at to the
             website.❌</p>
-
-        <p>📌Add a page for the schedule Archive ❌</p>
         <p>📌Add a page for links to all of my educational websites. Trailhead, ZTM, Google docs, calendar,
             Linkedin, Resumes, etc.❌</p>
-        <p>📌Add a MySQL database to archive schedules and notes. Add ability to create notes and edit schedules
+        <p>📌Add a MySQL OR Firebase database to archive schedules and notes. Add ability to create notes and edit schedules
             with this feature. ❌</p>
         <p>📌Add the ability to be able to toggle things as either complete or incomplete (also as a bonus, send
             completed tasks to the archive once they are toggled as complete) ❌</p>
@@ -16,17 +14,15 @@
         </p>
         <p>📌Add an agile methodology course ❌</p>
         <p>📌Add a public speaking class ❌</p>
-        <p>📌Look into starting trading classes again with Chart Champions and add that to the curriculum ❌</p>
         <p>📌Apply for some jobs with CivicPlus? ❌</p>
-        <p>📌Look into trading classes through Chart Champions. Add a spot for the courses in this website. ❌</p>
         <p>📌Add a section in my planning application that better organizes my google documents. As of now they are
             ridiculous and difficult to see whats what. Google Drive offers a solution but I want a better one for
             myself. Generate a page where I can more easily access my google documents. ❌</p>
-        <p>📌Add a login ability to my education application. This will help protect the data. NOTE: Hard-coding
+        <p>📌Add a 'login' ability to my education application. This will help protect the data. NOTE: Hard-coding
             everything every week will be a helpful tool to protect everything on Github as well as on my machine,
             locally. A weekly/bi-weekly backup may be warranted.❌</p>
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
+        <p>📌Add a reading list to the education application ❌</p>
+        <p>📌Add somt switching ability to the Home page of the education planner. Allow myself to be able to toggle between the different education paths, or see them all at once. ❌</p>
         <!-- <p>📌❌</p> -->
 
     </section>
@@ -35,5 +31,8 @@
         <h2>Archived Notes, Reminders, and Ideas</h2>
 
         <p>📌Add Handlebars, Vue, or React to this website to make it a multi page application (Vue or Handlebars isthe preffered). ✔️</p>
+        <p>📌Add a page for the schedule Archive ✔️</p>
+        <p>📌Look into starting trading classes again with Chart Champions and add that to the curriculum ✔️</p>
+
     </section>
 </template>
