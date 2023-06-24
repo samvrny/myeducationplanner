4186fae6a02b14b66a9f6a06f7ec78411518883c
samvrny <samvrny@gmail.com>
2023-06-24 14:05:01 -0500

    finished tweaking the notes page into having an li element instead of a p element for each note. Formatted it with CSS

--- src/views/Notes.vue
+++ src/views/Notes.vue
@@ -1,47 +1,51 @@
 <template>
-    <section class="notes path-box text-center">
-        <h2>Active Notes, Reminders, and Ideas</h2>
-        <p>📌Add Webfontloader to this website so I can add a prefferable font that I like to look at to the
-            website.❌</p>
-        <p>📌Add a page for links to all of my educational websites. Trailhead, ZTM, Google docs, calendar,
-            Linkedin, Resumes, etc.❌</p>
-        <p>📌Add a MySQL OR Firebase database to archive schedules and notes. Add ability to create notes and edit schedules
-            with this feature. ❌</p>
-        <p>📌Add the ability to be able to toggle things as either complete or incomplete (also as a bonus, send
-            completed tasks to the archive once they are toggled as complete) ❌</p>
-        <p>📌Add a scrum class to furthered courses ❌</p>
-        <p>📌Add a learning environment for all the Jargon in tech so I can learn what people are talking about ❌
-        </p>
-        <p>📌Add an agile methodology course ❌</p>
-        <p>📌Add a public speaking class ❌</p>
-        <p>📌Apply for some jobs with CivicPlus? ❌</p>
-        <p>📌Add a section in my planning application that better organizes my google documents. As of now they are
-            ridiculous and difficult to see whats what. Google Drive offers a solution but I want a better one for
-            myself. Generate a page where I can more easily access my google documents. ❌</p>
-        <p>📌Add a 'login' ability to my education application. This will help protect the data. NOTE: Hard-coding
-            everything every week will be a helpful tool to protect everything on Github as well as on my machine,
-            locally. A weekly/bi-weekly backup may be warranted.❌</p>
-        <p>📌Add a reading list to the education application ❌</p>
-        <p>📌Add somt switching ability to the Home page of the education planner. Allow myself to be able to toggle between the different education paths, or see them all at once. ❌</p>
-        <p>📌Add Tailwind to this project to get some better looking CSS going forward ❌</p>
-        <p>📌Add Firebase to the project to save archived notes and schedules. ❌</p>
-        <p>📌Give this website a rules for life section ❌</p>
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
-        <!-- <p>📌❌</p> -->
+    <section class="notes path-box">
+        <h2 class="text-center">Active Notes, Reminders, and Ideas</h2>
+        <ul>
+            <li>Add Webfontloader to this website so I can add a prefferable font that I like to look at to the
+                website. ❌</li>
+            <li>Add a page for links to all of my educational websites. Trailhead, ZTM, Google docs, calendar,
+                Linkedin, Resumes, etc. ❌</li>
+            <li>Add a MySQL OR Firebase database to archive schedules and notes. Add ability to create notes and edit
+                schedules
+                with this feature. ❌</li>
+            <li>Add the ability to be able to toggle things as either complete or incomplete (also as a bonus, send
+                completed tasks to the archive once they are toggled as complete) ❌</li>
+            <li>Add a scrum class to furthered courses ❌</li>
+            <li>Add a learning environment for all the Jargon in tech so I can learn what people are talking about ❌
+            </li>
+            <li>Add an agile methodology course ❌</li>
+            <li>Add a public speaking class ❌</li>
+            <li>Apply for some jobs with CivicPlus? ❌</li>
+            <li>Add a section in my planning application that better organizes my google documents. As of now they are
+                ridiculous and difficult to see whats what. Google Drive offers a solution but I want a better one for
+                myself. Generate a page where I can more easily access my google documents. ❌</li>
+            <li>Add a 'login' ability to my education application. This will help protect the data. NOTE: Hard-coding
+                everything every week will be a helpful tool to protect everything on Github as well as on my machine,
+                locally. A weekly/bi-weekly backup may be warranted.❌</li>
+            <li>Add a reading list to the education application ❌</li>
+            <li>Add some switching ability to the Home page of the education planner. Allow myself to be able to toggle
+                between the different education paths, or see them all at once. ❌</li>
+            <li>Add Tailwind to this project to get some better looking CSS going forward ❌</li>
+            <li>Add Firebase to the project to save archived notes and schedules. ❌</li>
+            <li>Give this website a rules for life section ❌</li>
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+            <!-- <li>❌</li> -->
+        </ul>
 
     </section>
 
     <section class="notes path-box text-center">
         <h2>Archived Notes, Reminders, and Ideas</h2>
 
-        <p>📌Add Handlebars, Vue, or React to this website to make it a multi page application (Vue or Handlebars isthe preffered). ✔️</p>
+        <p>📌Add Handlebars, Vue, or React to this website to make it a multi page application (Vue or Handlebars isthe
+            preffered). ✔️</p>
         <p>📌Add a page for the schedule Archive ✔️</p>
         <p>📌Look into starting trading classes again with Chart Champions and add that to the curriculum ✔️</p>
 
-    </section>
-</template>
+</section></template>
