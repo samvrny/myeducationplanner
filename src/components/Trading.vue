f76f4ae0e02571618e5afeb7f63653f31327148b
samvrny <samvrny@gmail.com>
2023-07-08 21:53:00 -0500

    got some more code for the trading page written

--- src/components/Trading.vue
+++ src/components/Trading.vue
@@ -3,18 +3,15 @@
     <div class="mt-20">
         <section class="trading-accounts w-3/6 inline-block text-center">
             <h3 class="mr-72 text-4xl">Accounts</h3>
-            <a href="" target="_blank"><button class="mt-10">Etrade</button></a>
+            <a href="https://us.etrade.com/home/welcome-back" target="_blank"><button class="mx-6 mt-10 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Etrade</button></a>
             <!-- <a class="mt-4 block" href="" target="_blank">EToro</a> -->
             <!-- <a class="mt-4 block" href="" target="_blank">Webull</a> -->
             <!-- <a class="mt-4 block" href="" target="_blank">Kraken</a> -->
-            <!-- <a class="mt-4 block" href="" target="_blank"></a> -->
         </section>
-        <section class="trading-charts w-3/6 inline-block align-top">
+        <section class="trading-charts w-3/6 inline-block text-center align-top">
             <h3 class="mr-72  text-4xl">Charts and Documents</h3>
             <!-- <i class="fa-sharp fa-solid fa-envelope"></i> -->
-            <!-- <a class="mt-4 block" href="" target="_blank">Trading View</a>
-            <a class="mt-4 block" href="" target="_blank"></a> -->
-            <!-- <a class="mt-4 block" href="" target="_blank"></a> -->
+            <a href="https://us.etrade.com/home/welcome-back" target="_blank"><button class="mx-6 mt-10 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Trading View</button></a>
         </section>
     </div>
 </template>
