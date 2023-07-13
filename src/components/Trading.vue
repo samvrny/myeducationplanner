832dad5c5e0c39a3e61b38e15cac6be4f0617508
samvrny <samvrny@gmail.com>
2023-07-13 17:53:35 -0500

    some modifications to the schedule. Added more options to the trading menu

--- src/components/Trading.vue
+++ src/components/Trading.vue
@@ -4,14 +4,17 @@
         <section class="trading-accounts w-3/6 inline-block text-center">
             <h3 class="mr-72 text-4xl">Accounts</h3>
             <a href="https://us.etrade.com/home/welcome-back" target="_blank"><button class="mx-6 mt-10 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Etrade</button></a>
-            <!-- <a class="mt-4 block" href="" target="_blank">EToro</a> -->
-            <!-- <a class="mt-4 block" href="" target="_blank">Webull</a> -->
+            <a class="mt-4 block" href="https://www.etoro.com/portfolio/overview" target="_blank"><button class="mx-6 mt-6 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Etoro</button></a>
+            <a class="mt-4 block" href="https://pro.kraken.com/app/trade/ltc-usd" target="_blank"><button class="mx-6 mt-6 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Kraken</button></a>
+            <a class="mt-4 block" href="https://www.coinbase.com/trade" target="_blank"><button class="mx-6 mt-6 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Coinbase</button></a>
             <!-- <a class="mt-4 block" href="" target="_blank">Kraken</a> -->
         </section>
         <section class="trading-charts w-3/6 inline-block text-center align-top">
             <h3 class="mr-72  text-4xl">Charts and Documents</h3>
             <!-- <i class="fa-sharp fa-solid fa-envelope"></i> -->
-            <a href="https://us.etrade.com/home/welcome-back" target="_blank"><button class="mx-6 mt-10 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Trading View</button></a>
+            <a href="https://www.tradingview.com/" target="_blank"><button class="mx-6 mt-10 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Trading View</button></a>
+            <a class="mt-4 block" href="https://coinmarketcap.com/" target="_blank"><button class="mx-6 mt-2 text-xl bg-purple-800 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-52">Coinmarketcap</button></a>
+            <a class="mt-4 block" href="https://docs.google.com/document/d/106-WAokogAjsSK790icOXY1hzGjozE-cp5PMz9C41N0/edit?usp=sharing" target="_blank"><button class="mx-6 mt-2 text-xl bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded w-52">Trading Notes</button></a>
         </section>
     </div>
 </template>
